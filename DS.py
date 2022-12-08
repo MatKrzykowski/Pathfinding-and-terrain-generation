8da28625567e6bcd11293899ecc13203f137432e
MKrzykow <matkrzykowski@gmail.com>
2022-12-08 19:44:15 +0100

    Move comments to README

--- DS.py
+++ DS.py
@@ -1,19 +1,4 @@
 # DS.py
-# Script generating heightmap using diamond-square (DSA) algorithm and finding
-# shortest path between two endpoints using Dijkstra's algorithm (DA).
-# https://en.wikipedia.org/wiki/Diamond-square_algorithm
-# https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
-#
-# Script was written as an exercise in path finding algorithms in summer 2016.
-#
-# Changelog:
-# 28.10.2016 - Script revision
-#
-# To do list:
-# - Replacing unvisited set with deque weighted with distance
-#   Look Python Cookbook chapter 1.5
-# - Introduce A* algorithm as an alternative and compare their effectiveness
-# - Introduce different map generation algorithms
 
 # Libraries import
 import numpy as np
@@ -300,6 +285,7 @@ def dijkstra(m=8, random_endpoints=False):
             break  # End the loop
 
     return hmap, endpoint  # Return for plotting
+
 
 if __name__ == "__main__":
     hmap, endpoint = dijkstra()
--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Pathfinding-and-terrain-generation
 
-Experiments from 2016 in terrain generation and pathfinding algorythms. May be extended in the future to include proper planet surface generation (erosion, tectonic plates?)
+
+Script generating heightmap using diamond-square (DSA) algorithm and finding
+shortest path between two endpoints using Dijkstra's algorithm (DA).
+https://en.wikipedia.org/wiki/Diamond-square_algorithm
+https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+
+Script was written as an exercise in path finding algorithms in summer 2016.
+
+To do list:
+- Replacing unvisited set with deque weighted with distance
+  Look Python Cookbook chapter 1.5
+- Introduce A* algorithm as an alternative and compare their effectiveness
+- Introduce different map generation algorithms
+- Introduce proper planet surface generation (erosion, tectonic plates?)
