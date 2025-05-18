d2a346545fe082b8b37fc0b74418902c1a9c3e9a
Mateusz Krzykowski <matkrzykowski@gmail.com>
2025-05-18 15:07:08 +0200

    Add type annotations to point.py

--- point.py
+++ point.py
@@ -1,12 +1,14 @@
 """point.py"""
 
+from typing import Iterable
+
 import numpy as np
 
 
 class Point:
     """Class describing single point on the heightmap."""
 
-    def __init__(self, z, x, y):
+    def __init__(self, z, x, y) -> None:
         """Initialization method"""
         self.x = x
         self.y = y
@@ -14,10 +16,10 @@ class Point:
         self.visited = False  # check if point was visited by DA yet
 
     @property
-    def pos(self):
+    def pos(self) -> Iterable[float]:
         """Returns 3D point position as numpy array"""
         return np.array([self.x, self.y, self.z])
 
-    def dist(self, other):
+    def dist(self, other) -> float:
         """Method calculating distance between two others given poth points."""
         return np.linalg.norm(self.pos - other.pos)
